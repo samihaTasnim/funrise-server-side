15831c6c5ed2b02aada854c95dcf313a7ba8db98
sam <samxtasnim@gmail.comgit confing --global user.emailgit config --global user.email samxtasnim@gmail.com>
2021-03-31 18:04:59 +0600

    halfway done

--- index.js
+++ index.js
@@ -16,6 +16,19 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db(`${process.env.DB_NAME}`).collection("toyInfo");
+
+  app.post('/addProduct', (req, res) => {
+    collection.insertOne(req.body)
+    .then(result => console.log(result)) 
+    .catch(err => console.log(err))
+  })
+
+  app.get('/toys', (req, res) => {
+    collection.find({})
+    .toArray((err, items) => {
+        res.send(items)
+    })
+})
   console.log('connected to database');
 });
 
